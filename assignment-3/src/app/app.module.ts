565b05b0f6232901ca43e5ba18f9460f29f70e46
Mohan <kmohan0910@gmail.com>
2022-09-24 08:42:13 +0530

    Error fix

--- assignment-3/src/app/app.module.ts
+++ assignment-3/src/app/app.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CreatelogComponent } from './createlog/createlog.component';
-import { DynamicUpdate } from './dynamicUpdate/dynamicupdate.component';
 @NgModule({
   declarations: [
-    AppComponent,
-    CreatelogComponent,
-    DynamicUpdate
+    AppComponent
   ],
   imports: [
     BrowserModule
